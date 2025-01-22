019217d9d1ae7328c1888af0d8daa74d52253845
Kamal-Bamma <Kamal.Bamma05@gmail.com>
2025-01-22 14:43:56 +0545

    finished frontend and backend product added and list product

---
+++ Backend/routes/productRoute.js
@@ -0,0 +1,25 @@
+import express from "express";
+
+import {
+  listProduct,
+  addProduct,
+  removeProduct,
+  singleProduct,
+} from "../controllers/productController.js";
+import upload from "../middleware/multer.js";
+
+const productRouter = express.Router();
+
+productRouter.post(
+  "/add",
+  upload.fields([
+    { name: "image", maxCount: 1 },
+    { name: "video", maxCount: 1 },
+  ]),
+  addProduct
+);
+productRouter.post("/single", singleProduct);
+productRouter.post("/remove", removeProduct);
+productRouter.get("/list", listProduct);
+
+export default productRouter;
